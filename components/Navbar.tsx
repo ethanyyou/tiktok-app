b52d98589a8cc4c400c0314bd35cc311d78ccf98
icecoffee98 <yixuanyu98@gmail.com>
2023-01-09 19:33:29 -0800

    intergrated Google Login method

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 
 import Logo from '/titik-logo.png';
 const Navbar = () => {
+  const user = false;
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href='#'>
@@ -15,6 +18,17 @@ const Navbar = () => {
           <Image className='cursor-pointer' src='/tiktik-logo.png' alt='Tiktik' fill />
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log('Error')}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import NoResults from '../components/NoResults';
 import VideoCard from '../components/VideoCard';
+
 import type { Video } from '../types';
 
 interface IProps {
