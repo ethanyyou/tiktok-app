d76c6c0dd561c78ff5f29e310f12e10bce6ece1e
icecoffee98 <yixuanyu98@gmail.com>
2023-01-11 09:20:07 -0800

    configure nextjs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['s2.loli.net', 'lh3.googleusercontent.com'],
