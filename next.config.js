e2e560ee4ecf9fb6810015a71365629a7cf08fd6
icecoffee98 <yixuanyu98@gmail.com>
2023-01-11 21:08:42 -0800

    ...

--- next.config.js
+++ next.config.js
@@ -6,7 +6,13 @@ const nextConfig = {
 
   reactStrictMode: true,
   images: {
-    domains: ['s2.loli.net', 'lh3.googleusercontent.com'],
+    domains: [
+      's2.loli.net',
+      'lh3.googleusercontent.com',
+      'https://tiktok-qbijm2dug-easianyu.vercel.app',
+      'https://tiktok-app-eosin.vercel.app',
+      'https://tiktok-eji6j9wj9-easianyu.vercel.app',
+    ],
   },
 };
 
